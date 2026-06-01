f2a2f3c68c2a0203e4bf3118db3fde7791335aea
Pranoti <bartakkepranoti@gmail.com>
2026-06-01 23:27:49 +0530

    Fetched artists data

---
+++ ingestion/fetch_artists.py
@@ -0,0 +1,38 @@
+import requests
+import json
+from datetime import datetime
+
+url="https://musicbrainz.org/ws/2/artist"
+artists = [
+    "Shreya Ghoshal",
+    "Arijit Singh",
+    "Sonu Nigam",
+    "KK",
+    "Sunidhi Chauhan"
+]
+headers = {
+    "User-Agent": "MusicAnalyticsETL/1.0 (bartakkepranoti14@gmail.com)" 
+}
+
+all_artists = []
+
+for artist in artists:
+    params={
+    "query":f"artist:{artist}",
+    "fmt":"json"
+   }
+    response = requests.get(url,params=params,headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        all_artists.extend(data["artists"])
+
+current_date = datetime.now().strftime("%Y_%m_%d")
+file_path = f"data/raw/artists/artists_{current_date}.json"
+
+try:
+    with open(file_path,"w") as file:
+        json.dump(all_artists, file , indent=4)
+    print("file saved successfully") 
+
+except Exception as e:
+    print(f"Error occurred while writing to file: {e}")
