0ded3ca84a64b03d0996984ea9923a9839adfefd
horizonchaser12 <suryakant.mangaraj@gmail.com>
2026-06-03 21:29:58 +0530

    API Check

--- ingestion/check_api.py
+++ ingestion/check_api.py
@@ -1,33 +1,84 @@
-from urllib import response
-
-from matplotlib.font_manager import json_dump
-import requests 
+import requests
 import json
+from datetime import datetime
+import logging
+import os
 
-# API I am working on user, country and location
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(levelname)s: %(message)s"
+)
+logger = logging.getLogger(__name__)
 
+url = "https://musicbrainz.org/ws/2/artist"
 
-# resp = requests.post("http://localhost:4000/v1/user/register", json={"username": "","email": "","password": ""})
-# print(resp.status_code)
-# print(resp.headers.get("content-type"))
-# print(resp.text[:400])
-
-# data = resp.json()
-# print(data)
-
-# url = "http://localhost:4000/user/register"
-
-
-# data = {
+artists = [
+    "Coldplay",
+    "Lord Huron",
+    "Zara Larsson"
     
-# }
+]
 
-# response = requests.post(url, json=data)
+headers = {
+    "User-Agent": "MusicAnalyticsETL/1.0 (suryakant.mangaraj@gmail.com)"
+}
 
-# print(response.status_code)
-# print(response.json())
+all_artists = []
 
+try:
+    for artist in artists:
 
+        params = {
+            "query": f'artist:"{artist}"',
+            "fmt": "json",
+            "limit": 1
+        }
 
-response = requests.get("https://musicbrainz.org/ws/2/artist/?query=the+local+train&fmt=json")
-print(response.json())
+        logger.info(f"Fetching {artist}")
+
+        response = requests.get(
+            url,
+            params=params,
+            headers=headers,
+            timeout=30
+        )
+
+        response.raise_for_status()
+
+        data = response.json()
+
+        if not data.get("artists"):
+            logger.warning(f"No result found for {artist}")
+            continue
+
+        artist_data = data["artists"][0]
+
+        cleaned_artist = {
+            "artist_id": artist_data.get("id"),
+            "artist_name": artist_data.get("name"),
+            "artist_type": artist_data.get("type"),
+            "country": artist_data.get("country"),
+            "disambiguation": artist_data.get("disambiguation"),
+            "score": artist_data.get("score")
+        }
+
+        all_artists.append(cleaned_artist)
+
+except Exception as e:
+    logger.error(f"Error: {e}")
+
+os.makedirs("data/raw/artists", exist_ok=True)
+
+current_date = datetime.now().strftime("%Y_%m_%d")
+
+file_path = f"data/raw/artists/artists_{current_date}.json"
+
+with open(file_path, "w", encoding="utf-8") as file:
+    json.dump(
+        all_artists,
+        file,
+        indent=4,
+        ensure_ascii=False
+    )
+
+logger.info(f"Saved to {file_path}")
