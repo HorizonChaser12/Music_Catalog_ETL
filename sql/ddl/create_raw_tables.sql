1481b01b2ec51b98b6e8c57d219a0caafcddaf90
Pranoti <bartakkepranoti@gmail.com>
2026-06-05 10:23:35 +0530

    Created a table and loaded data into it

--- sql/ddl/create_raw_tables.sql
+++ sql/ddl/create_raw_tables.sql
@@ -1,6 +1,9 @@
-CREATE TABLE IF NOT EXISTS Artists (
-    id TEXT PRIMARY KEY,
-    name TEXT NOT NULL,
-    gender TEXT,
-    country TEXT,
-    score INTEGER);  
+CREATE TABLE IF NOT EXISTS public.artists
+(
+    id text COLLATE pg_catalog."default" NOT NULL,
+    name text COLLATE pg_catalog."default" NOT NULL,
+    gender text COLLATE pg_catalog."default",
+    country text COLLATE pg_catalog."default",
+    score integer,
+    CONSTRAINT artists_pkey PRIMARY KEY (id)
+)
