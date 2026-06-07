8ead97cd3eeb8b72dfb8201678d14cb49c0c35c4
Pranoti <bartakkepranoti@gmail.com>
2026-06-07 16:52:13 +0530

    changed the volume mapping in docker config file

--- database/setup_database.py
+++
@@ -1,5 +0,0 @@
-from execute_sql_file import execute_sql_file
-
-execute_sql_file(
-    "/opt/project/sql/ddl/create_raw_tables.sql"
-)
--- ingestion/fetch_release.py
+++ ingestion/fetch_release.py
@@ -3,6 +3,7 @@ import requests
 import json
 from datetime import datetime
 import time
+import time
 
 logging.basicConfig(
     level=logging.INFO,
@@ -24,30 +25,20 @@ def fetch_releases():
     
     logging.info("Starting release extraction")
 
-    all_releases = []
-    for artist_name in artists:
-        params={
-            "query": f"artist:{artist_name}",
-        "fmt" : "json" 
-       }
-        logging.info(f"Fetching data for artist: {artist_name}")
-        response = requests.get(url,params=params,headers=headers)
-        if response.status_code==200:
-          data = response.json()
-          logging.info(f"Status code {response.status_code} received. Processing data for artist: {artist_name}")
-          all_releases.extend(data.get("releases", []))
-          release_count = len(data.get("releases", []))
-          logging.info(f"Fetched {release_count} releases for {artist_name}")
-        else:
-           logging.error(f"Failed to fetch data for {artist_name}.status code: {response.status_code}")
-        time.sleep(1)
-    current_date = datetime.now().strftime("%Y_%m_%d")
-    file_path = f"/opt/project/data/raw/releases/releases_{current_date}.json"
+all_releases = []
 
-    try:
-        with open(file_path,"w") as f:   
-          json.dump(all_releases,f,indent=4)
-        logging.info(f"File saved successfully at {file_path}") 
-    except Exception as e:
-       logging.error(f"Error occurred while writing the file:{e}") 
+release_id = "c9a8647a-d97c-4982-bf20-3ede7b39f568"
+url = f"https://musicbrainz.org/ws/2/release/{release_id}"
 
+params = {
+    "inc": "recordings",
+    "fmt": "json"
+}
+
+response = requests.get(
+    url,
+    params=params,
+    headers=headers
+)
+
+print(json.dumps(response.json(), indent=4))
