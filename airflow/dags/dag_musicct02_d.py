a26cb6dd838b62550c45874c97138d5456fd5b94
Pranoti <bartakkepranoti@gmail.com>
2026-06-26 22:30:16 +0530

    added sanitised task dependency in dag

--- airflow/dags/dag_musicct02_d.py
+++ airflow/dags/dag_musicct02_d.py
@@ -25,26 +25,34 @@ with DAG(
         task_id="extract_api_data",
         bash_command="python3 /opt/project/loading/setup_database.py && python3 /opt/project/ingestion/fetch_all_data.py"
     )
-    load_lnd_artists=BashOperator(
-        task_id="load_lnd_artists",
+    lnd_artists_load=BashOperator(
+        task_id="lnd_artists_load",
         cwd="/opt/project/loading",
         bash_command="python3 load_tables_landing.py artists landing lnd_artists"
     )
-    load_lnd_releases=BashOperator(
-        task_id="load_lnd_releases",
+    lnd_releases_load=BashOperator(
+        task_id="lnd_releases_load",
         cwd="/opt/project/loading",
         bash_command="python3 load_tables_landing.py releases landing lnd_releases "
     ) 
-    load_lnd_recordings=BashOperator(
-        task_id="load_lnd_recordings",
+    lnd_recordings_load=BashOperator(
+        task_id="lnd_recordings_load",
         cwd="/opt/project/loading",
         bash_command="python3 load_tables_landing.py recordings landing lnd_recordings"
     )
-    load_lnd_urls=BashOperator(
-        task_id="load_lnd_urls",
+    lnd_urls_load=BashOperator(
+        task_id="lnd_urls_load",
         cwd="/opt/project/loading",
         bash_command="python3 load_tables_landing.py urls landing lnd_urls"
     )
-    extract_api_data >>[load_lnd_artists, load_lnd_releases, load_lnd_recordings, load_lnd_urls]
+    san_artists_load = BashOperator(
+        task_id='san_artists_load',
+        bash_command="""
+        /opt/spark/bin/spark-submit \
+        --master spark://spark-master:7077 \
+        /opt/project/transformation/sanitize_artists.py
+        """
+    )
+    extract_api_data >>[lnd_artists_load, lnd_releases_load, lnd_recordings_load, lnd_urls_load,san_artists_load]
 
     
